44b620071ab18d58186b73848b511162827b4799
tenorok <mail@tenorok.ru>
2021-06-17 16:48:41 +0300

    Запуск prometheus только вне режима разработки.

--- generators/app/templates/monitoring/common/src/Prometheus.ts
+++ generators/app/templates/monitoring/common/src/Prometheus.ts
@@ -5,16 +5,18 @@ import express = require('express');
 import { createLogger } from './Logger';
 const log = createLogger('Prometheus');
 
-const prom = express();
+export function createPrometheus(): void {
+    const prom = express();
 
-client.collectDefaultMetrics();
+    client.collectDefaultMetrics();
 
-prom.get('/metrics', async (_, res) => {
-    res.set('Content-Type', client.register.contentType);
-    res.end(await client.register.metrics());
-});
+    prom.get('/metrics', async (_, res) => {
+        res.set('Content-Type', client.register.contentType);
+        res.end(await client.register.metrics());
+    });
 
-const port = config.get<number>('prometheus.port');
-prom.listen(port, () => {
-    log.info(`Process listening at port ${port}`);
-});
+    const port = config.get<number>('prometheus.port');
+    prom.listen(port, () => {
+        log.info(`Process listening at port ${port}`);
+    });
+}
