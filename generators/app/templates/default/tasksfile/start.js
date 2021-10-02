4839ea91219fd59214e11343b15a17e6f4baacd8
tenorok <mail@tenorok.ru>
2021-10-02 15:38:52 +0300

    task: Возможность отключить DEBUG при старте.

--- generators/app/templates/default/tasksfile/start.js
+++ generators/app/templates/default/tasksfile/start.js
@@ -1,30 +1,46 @@
-const { sh, help } = require('./_utils');
+const { sh, help, Command } = require('./_utils');
 
-function runStart(command) {
-    const debug = [
-        '-follow-redirects',
-    ];
+function runStart(command, options) {
+    const { nodebug } = options;
+    const debug = ['-follow-redirects'];
 
-    sh([
-        'DEBUG=*,' + debug.join(','),
-        'NODE_ENV=development',
-        'TS_NODE_LOG_ERROR=true',
-        command,
-        '| npx bunyan --time local',
-    ].join(' '));
+    const cmd = new Command(command + ' | npx bunyan --time local');
+
+    if (!nodebug) {
+        cmd.env('DEBUG', '*,' + debug.join(','));
+    }
+
+    cmd.env('NODE_ENV', 'development');
+    cmd.env('TS_NODE_LOG_ERROR', 'true');
+
+    sh(cmd.get());
 }
 
 const start = {
-    default() {
-        runStart('ts-node --require tsconfig-paths/register ./app/src/index.ts');
+    default(options) {
+        runStart(
+            'ts-node --require tsconfig-paths/register ./app/src/index.ts',
+            options,
+        );
     },
 
-    watch() {
-        runStart('nodemon --exec ./node_modules/.bin/ts-node -- --require tsconfig-paths/register ./app/src/index.ts');
+    watch(options) {
+        runStart(
+            'nodemon --exec ./node_modules/.bin/ts-node -- --require tsconfig-paths/register ./app/src/index.ts',
+            options,
+        );
     },
 };
 
-help(start.default, 'Start application');
-help(start.watch, 'Start application and restart on every changing');
+help(start.default, 'Start application', {
+    options: {
+        nodebug: 'Disable DEBUG logs',
+    },
+});
+help(start.watch, 'Start application and restart on every changing', {
+    options: {
+        nodebug: 'Disable DEBUG logs',
+    },
+});
 
 module.exports = start;
