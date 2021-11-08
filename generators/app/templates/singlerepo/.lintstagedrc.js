b747b42978bfe244c44f850738b7188f2d982ec3
tenorok <mail@tenorok.ru>
2021-11-08 11:22:44 +0300

    madge: Конфиг.
    vscode/launch: Корректный путь до входящего файла.

--- generators/app/templates/singlerepo/.lintstagedrc.js
+++ generators/app/templates/singlerepo/.lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
     "*.{js,jsx,ts,tsx}": "eslint",
     "*.{js,jsx,ts,tsx,json,html,css,scss,md}": "prettier --write",
     '*': () => [
-        'madge --circular --extensions ts ./app/src/',
+        'madge --circular ./app/src/',
         'tsc -p tsconfig.json --incremental false --noEmit',
     ],
 }
